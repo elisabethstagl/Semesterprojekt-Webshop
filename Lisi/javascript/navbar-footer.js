6da19d67cb600e984ff2585abfd56de5e2683172
ElisabethStagl <e.stagl@hotmail.com>
2023-01-25 15:59:58 +0100

    zeugs

---
+++ Lisi/javascript/navbar-footer.js
@@ -0,0 +1,17 @@
+fetch('navbar.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+fetch('footer.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_footer");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
