76f837b22d145130a04619a9871376731ec73279
ElisabethStagl <e.stagl@hotmail.com>
2023-02-02 10:50:38 +0100

    navbar und footer load

--- Lisi/javascript/main.js
+++ Lisi/javascript/main.js
@@ -1,26 +1,4 @@
-var navbar = document.querySelector("#navbar");
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    navbar.innerHTML = this.responseText;
-  }
-};
-xhttp.open("GET", "navbar.html", true);
-xhttp.send();
 
-// var pathname = window.location.pathname;
-// var activeLink = document.querySelector("#navbar a[href='" + pathname + "']");
-// if (activeLink) {
-//   activeLink.classList.add("active");
-// } active funktioniert nicht, wieso?
+$("#navbar").load("../Lisi/navbar.html");
 
-
-var footer = document.querySelector("#footer");
-   var xhttp = new XMLHttpRequest();
-   xhttp.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-       footer.innerHTML = this.responseText;
-     }
-   };
-   xhttp.open("GET", "footer.html", true);
-   xhttp.send();
+$("#footer").load("../Lisi/footer.html");
