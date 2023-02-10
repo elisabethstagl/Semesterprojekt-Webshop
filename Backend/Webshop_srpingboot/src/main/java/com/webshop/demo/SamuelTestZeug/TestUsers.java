dd4397dc33f28033f7591c455d898723fd433f92
Encelatus <samuel.tenschert@gmail.com>
2023-02-10 10:02:59 +0100

    small fix

--- Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/SamuelTestZeug/TestUsers.java
+++ Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/SamuelTestZeug/TestUsers.java
@@ -1,12 +1,13 @@
 package com.webshop.demo.SamuelTestZeug;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 public class TestUsers {
