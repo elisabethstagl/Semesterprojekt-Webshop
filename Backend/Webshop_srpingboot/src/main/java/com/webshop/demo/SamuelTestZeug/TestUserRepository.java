465e31af298beeceba52a4301223e0da3e3d9727
Encelatus <samuel.tenschert@gmail.com>
2023-07-14 10:39:35 +0200

    deleted "SamuelTestZeug"

--- Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/SamuelTestZeug/TestUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webshop.demo.SamuelTestZeug;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestUserRepository extends JpaRepository<TestUsers, Long>{
-    
-}
