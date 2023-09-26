24d04fa28eeb0f192df0d5ec3c8e1da1e4e7b4b8
ElisabethStagl <e.stagl@hotmail.com>
2023-09-26 19:27:42 +0200

    see all users added

--- Frontend/javascript/alle-user.js
+++ Frontend/javascript/alle-user.js
@@ -29,38 +29,38 @@ function displayUsers(users) {
     }
 }
 
-// Funktion zum Erstellen einer Produktkarte
-// Funktion zum Erstellen einer vertikalen Produktkarte
-// Funktion zum Erstellen einer vertikalen Produktkarte
 function createUserCard(user) {
-        // Erstelle die HTML-Struktur für eine Benutzerkarte
-        const card = $('<div class="col-lg-3 col-md-4 col-sm-6 mb-4"></div>'); // 4 Karten auf Desktops, 3 auf Tablets, 2 auf Handys
-        const cardInner = $('<div class="card mx-auto mb-5 card-products" style="background-color:transparent;"></div>');
-        const cardBody = $('<div class="card-body"></div>');
-        
-        // Benutzerinformationen in die Karte einfügen
-        const username = $(`<h5 class="card-title">Username: ${user.username}</h5>`);
-        const fullName = $(`<p class="card-text">Name: ${user.firstName} ${user.lastName}</p>`);
-        const sex = $(`<p class="card-text">Geschlecht: ${user.sex}</p>`);
-        const address = $(`<p class="card-text">Adresse: ${user.address} ${user.doornumber}, ${user.postalCode} ${user.city}</p>`);
-        const email = $(`<p class="card-text">Email: ${user.email}</p>`);
-        const role = $(`<p class="card-text">Email: ${user.role}</p>`);
-        const profilePicture = $(`<p class="card-text">Email: ${user.profilePicture}</p>`);
-        // Fügen Sie hier weitere Benutzerinformationen hinzu, wie Sie möchten.
+    // Erstelle die HTML-Struktur für eine Benutzerkarte
+    const card = $('<div class="col-lg-3 col-md-4 col-sm-6 mb-4"></div>'); // 4 Karten auf Desktops, 3 auf Tablets, 2 auf Handys
+    const cardInner = $('<div class="card mx-auto mb-5 card-products" style="background-color:transparent;"></div>');
+    const cardBody = $('<div class="card-body"></div>');
     
-        // Füge die HTML-Elemente zur Benutzerkarte hinzu
-        cardBody.append(username);
-        cardBody.append(fullName);
-        cardBody.append(sex);
-        cardBody.append(address);
-        cardBody.append(email);
-        cardBody.append(role);
-        // Fügen Sie weitere Informationen hinzu, wie oben beschrieben.
+    // Benutzerinformationen in die Karte einfügen
+    const username = $(`<h5 class="card-title">Username: ${user.username}</h5>`);
+    const fullName = $(`<p class="card-text">Name: ${user.firstName} ${user.lastName}</p>`);
+    const sex = $(`<p class="card-text">Geschlecht: ${user.sex}</p>`);
+    const address = $(`<p class="card-text">Adresse: ${user.address} ${user.doornumber}, ${user.postalCode} ${user.city}</p>`);
+    const email = $(`<p class="card-text">Email: ${user.email}</p>`);
+    const role = $(`<p class="card-text">Rolle: ${user.role}</p>`);
     
-        cardInner.append(cardBody);
-        card.append(cardInner);
+    // Überprüfe, ob ein Profilbild vorhanden ist, und füge es zur Karte hinzu, falls verfügbar
+    if (user.profilePicture) {
+        const profilePicture = $(`<img src="${user.profilePicture}" alt="Profilbild" class="card-img-top">`);
+        cardBody.append(profilePicture);
+    }
     
-        return card;
+    // Fügen Sie die restlichen Informationen zur Karte hinzu
+    cardBody.append(username);
+    cardBody.append(fullName);
+    cardBody.append(sex);
+    cardBody.append(address);
+    cardBody.append(email);
+    cardBody.append(role);
+    
+    cardInner.append(cardBody);
+    card.append(cardInner);
+
+    return card;
 }
 
 // Rufe die Funktion zum Laden der Produkte auf, wenn die Seite geladen ist
@@ -68,3 +68,21 @@ $(document).ready(function () {
     
         loadUsers();
 });
+
+
+// CHECKEN IF ADMIN
+// success: function (users) {
+//     // Überprüfe, ob der Benutzer die Rolle "admin" hat
+//     if (users && users.length > 0 && users[0].role === 'admin') {
+//         // Wenn ja, zeige die Benutzerdaten an
+//         displayUsers(users);
+//     } else {
+//         // Andernfalls, zeige eine Fehlermeldung
+//         displayErrorMessage('Nur Admins dürfen diese Seite sehen.');
+//     }
+// },
+// error: function (error) {
+//     console.error("Fehler beim Laden der User:", error);
+//     // Zeige auch hier eine Fehlermeldung an
+//     displayErrorMessage('Fehler beim Laden der Benutzerdaten.');
+// }
