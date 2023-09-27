4e82b4d9b573b356272873133329b9438f6d8f41
ElisabethStagl <e.stagl@hotmail.com>
2023-09-27 20:04:28 +0200

    test is back but still not working ?

---
+++ Backend/Webshop_srpingboot/src/test/java/com/webshop/FhWebshopApplicationTest.java
@@ -0,0 +1,16 @@
+package com.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+
+@SpringBootTest
+@TestPropertySource("application-test.properties")
+class FhWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
