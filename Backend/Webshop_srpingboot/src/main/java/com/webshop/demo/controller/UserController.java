ada323d363460b48b31af48e06b2e35b4ccbdfa2
Encelatus <samuel.tenschert@gmail.com>
2023-09-28 21:03:35 +0200

    removed unused imports

--- Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/controller/UserController.java
+++ Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.webshop.demo.dto.LoginRequest;
 import com.webshop.demo.dto.RegistrationRequest;
