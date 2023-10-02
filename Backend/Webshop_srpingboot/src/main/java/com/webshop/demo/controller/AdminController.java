49297913d47d205e7712f4299b748dffd1c5b0f6
Encelatus <samuel.tenschert@gmail.com>
2023-10-02 16:02:46 +0200

    admin edit user changes

--- Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/controller/AdminController.java
+++ Backend/Webshop_srpingboot/src/main/java/com/webshop/demo/controller/AdminController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -37,6 +39,16 @@ public class AdminController {
     public ResponseEntity<List<User>> listUsers() {
         List<User> users = userService.findAll();
         return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        try {
+            User user = userService.update(id, updatedUser);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     // Endpoint to get user details by ID
